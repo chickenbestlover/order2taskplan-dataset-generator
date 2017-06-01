c71081ec30c9a93f67c7bc9fe69aa119945c0ce9
chickenbestlover <jmpark@rit.kaist.ac.kr>
2017-06-02 02:28:20 +0900

    latest

--- order-environment-taskplan_generator.py
+++ order-environment-taskplan_generator.py
@@ -9,6 +9,7 @@ files_toRead = glob.glob('../data/order-environment-taskplan*.txt')
 
 file_train = open('../data/order-environment-taskplan-train.txt', 'w')
 file_test = open('../data/order-environment-taskplan-test.txt','w')
+file_whole = open('../data/order-environment-taskplan-whole.txt','w')
 pairs = []
 for file in files_toRead:
     file_toRead = open(file,'r')
@@ -19,10 +20,13 @@ for file in files_toRead:
 random.shuffle(pairs)
 pairs_train = pairs[:45000]
 pairs_test = pairs[45000:]
+for pair in pairs:
+    file_whole.writelines(pair)
 for pair in pairs_train:
     file_train.writelines(pair)
 for pair in pairs_test:
     file_test.writelines(pair)
 
+file_whole.close()
 file_train.close()
 file_test.close()
